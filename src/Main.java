6c0f12d7db02c36d75d7d7e2d98862dd0b6b3cde
BT_2S14_27 <email@email>
2024-09-18 10:14:45 -0400

    added dollar signs and comments

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
 
+        //Welcoming User
         System.out.println("Welcome to Tip calculator!");
-        System.out.print("Please enter your bill amount: ");
+
+        //Taking inputs from user of bill amount, number of people. and tip percentage
+        System.out.print("Please enter your bill amount: $");
         double billAmount = k.nextDouble();
         System.out.print("Please enter the total amount of people in your party: ");
         int peopleCount = k.nextInt();
@@ -12,26 +15,29 @@ public class Main {
         double tipPercent = k.nextInt();
         System.out.println("-----------------------------------------------------------------");
 
+        //Calculations of tips, total bill, and amounts needs to be paid per person
         double convertTipPercent = tipPercent / 100;
         double totalTips = billAmount * convertTipPercent;
         double totalBill = billAmount + totalTips;
         double tipsPerPerson = totalTips / peopleCount;
         double totalPerPerson = totalBill / peopleCount;
 
-        System.out.print("The amount each person has to pay is: ");
+        //Prints out all the values as receipt
+        System.out.print("The amount each person has to pay is: $");
         System.out.printf("%.2f", totalPerPerson);
         System.out.println();
-        System.out.print("The total bill amount is: ");
+        System.out.print("The total bill amount is: $");
         System.out.printf("%.2f", totalBill);
         System.out.println();
-        System.out.print("The tip amount each person pay is : ");
+        System.out.print("The tip amount each person pay is : $");
         System.out.printf("%.2f", tipsPerPerson);
         System.out.println();
-        System.out.print("The total tip amount is: ");
+        System.out.print("The total tip amount is: $");
         System.out.printf("%.2f", totalTips);
         System.out.println();
         System.out.println("-----------------------------------------------------------------");
-        System.out.print("Thank you for using the Tip Calculator!");
 
+        //Indication to the end of program
+        System.out.print("Thank you for using the Tip Calculator!");
         }
     }
